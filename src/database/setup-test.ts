6d543dafe31d5d4e018b9ac64fdd15ef9c35de51
Samuel Ferreira <samuel.ferreira.ti@gmail.com>
2024-09-28 13:03:39 -0300

    category test

--- src/database/setup-test.ts
+++ src/database/setup-test.ts
@@ -4,11 +4,13 @@ import { AppDataSource } from "./data-source";
 
 beforeAll(async () => {
   await AppDataSource.initialize();
+  console.log('Database inicialized');
 });
 
 afterAll(async () => {
   if (AppDataSource.isInitialized) {
     await AppDataSource.destroy();
+    console.log('Database destroyed');
   }
 });
 
--- src/entities/tests/categories.test.ts
+++ src/entities/tests/categories.test.ts
@@ -7,9 +7,51 @@ describe('Category Entity', () => {
     const category = new Categories();
     category.description = 'Test Category';
 
-    const savedCategory = await AppDataSource.manager.save(category);
+    const saved = await AppDataSource.manager.save(category);
 
-    expect(savedCategory).toHaveProperty('id');
-    expect(savedCategory.description).toBe('Test Category');
+    expect(saved).toHaveProperty('id');
+    expect(saved.description).toBe('Test Category');
+  });
+  it('should update a category', async () => {
+    const category = new Categories();
+    category.description = 'Test Category';
+
+    const saved = await AppDataSource.manager.save(category);
+
+    await AppDataSource
+    .manager
+    .update(
+      Categories, 
+      { id: saved.id }, 
+      { description: 'Test Update Category' })
+
+    const select = await AppDataSource
+    .getRepository(Categories)
+    .createQueryBuilder("categories")
+    .where("categories.id = :id", { id: saved.id })
+    .getOne()
+    
+    expect(select?.description).toBe('Test Update Category');
+  });
+  it('should delete a category', async () => {
+    const category = new Categories();
+    category.description = 'Test Delete Category';
+
+    const saved = await AppDataSource.manager.save(category);
+    const savedId = saved.id;
+
+    await AppDataSource
+    .manager
+    .delete(
+      Categories, 
+      { id: savedId })
+
+    const salect = await AppDataSource
+    .getRepository(Categories)
+    .createQueryBuilder("categories")
+    .where("categories.id = :id", { id: savedId })
+    .getOne()
+    
+    expect(salect).toBeNull();
   });
 });
